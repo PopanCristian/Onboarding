4f01059aa29bcbf6b7c820aea7db19cdf71a9150
PopanCristian <p_cristi10@yahoo.com>
2025-05-20 13:07:06 +0300

    testing

--- playwright_pytest_tasks.py
+++ playwright_pytest_tasks.py
@@ -1,4 +1,5 @@
 from playwright.sync_api import sync_playwright, Playwright
+import pytest
 FULL_NAME = 'Connor McGregor'
 EMAIL = 'theonetheonly@yahoo.com'
 CURRENT_ADDRESS = 'Ireland'
@@ -6,41 +7,72 @@ PERMANENT_ADDRESS = 'Around the globe'
 
 
 
-def run(playwright: Playwright):
-    browser = playwright.chromium.launch(headless=False, slow_mo=10)
-    page = browser.new_page()
-
-    page.goto("https://demoqa.com/")
-    page.locator("//div[contains(@class, 'top-card') and .//h5[text()='Elements']]").click()
-    page.locator("//li[contains(@class, 'btn-light') and .//span[text()='Text Box']]").click()
-    full_name = page.locator("//input[@id='userName']").fill(FULL_NAME)
-    email = page.locator("//input[@id='userEmail']").fill(EMAIL)
-    current_adress = page.locator("//textarea[@id='currentAddress']").fill(CURRENT_ADDRESS)
-    permanent_adress = page.locator("//textarea[@id='permanentAddress']").fill(PERMANENT_ADDRESS)
-
-    page.locator("//button[@id='submit']").click()
-
-    output_full_name = page.locator("//div[contains(@class,'col-md-12 col-sm-1')]/"
-                                    "p[@id='name']").inner_text().replace("Name:", "")
-    output_email = page.locator("//div[contains(@class,'col-md-12 col-sm-1')]/"
-                                "p[@id='email']").inner_text().replace("Email:", "")
-    output_current_address = page.locator("//div[contains(@class,'col-md-12 col-sm-1')]/"
-                                          "p[@id='currentAddress']").inner_text().replace("Current Address :", "")
-    output_permanent_address = page.locator("//div[contains(@class,'col-md-12 col-sm-1')]"
-                                            "/p[@id='permanentAddress']").inner_text().replace("Permananet Address :", "")
-
-    initial_list = [FULL_NAME,EMAIL,CURRENT_ADDRESS,PERMANENT_ADDRESS]
-    output_list = [output_full_name,output_email,output_current_address,output_permanent_address]
-    same_data = lambda: initial_list == output_list
-    assert same_data(), "They are not the same"
-    print("They are the same !")
-    browser.close()
-
-
-with sync_playwright() as p:
-    run(p)
+# def run(playwright: Playwright):
+#     browser = playwright.chromium.launch(headless=False, slow_mo=10)
+#     page = browser.new_page()
+#
+#     page.goto("https://demoqa.com/")
+#     page.locator("//div[contains(@class, 'top-card') and .//h5[text()='Elements']]").click()
+#     page.locator("//li[contains(@class, 'btn-light') and .//span[text()='Text Box']]").click()
+#     full_name = page.locator("//input[@id='userName']").fill(FULL_NAME)
+#     email = page.locator("//input[@id='userEmail']").fill(EMAIL)
+#     current_adress = page.locator("//textarea[@id='currentAddress']").fill(CURRENT_ADDRESS)
+#     permanent_adress = page.locator("//textarea[@id='permanentAddress']").fill(PERMANENT_ADDRESS)
+#
+#     page.locator("//button[@id='submit']").click()
+#
+#     output_full_name = page.locator("//div[contains(@class,'col-md-12 col-sm-1')]/"
+#                                     "p[@id='name']").inner_text().replace("Name:", "")
+#     output_email = page.locator("//div[contains(@class,'col-md-12 col-sm-1')]/"
+#                                 "p[@id='email']").inner_text().replace("Email:", "")
+#     output_current_address = page.locator("//div[contains(@class,'col-md-12 col-sm-1')]/"
+#                                           "p[@id='currentAddress']").inner_text().replace("Current Address :", "")
+#     output_permanent_address = (page.locator("//div[contains(@class,'col-md-12 col-sm-1')]"
+#                                             "/p[@id='permanentAddress']").inner_text().replace("Permananet "
+#                                                                                                "Address :", ""))
+#
+#     initial_list = [FULL_NAME, EMAIL, CURRENT_ADDRESS, PERMANENT_ADDRESS]
+#     output_list = [output_full_name, output_email, output_current_address, output_permanent_address]
+#     same_data = lambda: initial_list == output_list
+#     assert same_data(), "They are not the same"
+#     print("They are the same !")
+#     browser.close()
 
 
+# with sync_playwright() as p:
+#     run(p)
+
+URL = "https://demoqa.com/"
 
 
 
+@pytest.fixture
+def select(page, button):
+    return page.locator(button).clicked()
+
+
+@pytest.fixture
+def fill_context(page, button, context):
+    return page.page.locator(button).fill(context)
+
+
+class TestClass:
+    def __init__(self):
+        self.page = None
+        self.url = URL
+
+    @pytest.fixture(autouse=True, scope="function")
+    def open_close_browser(self):
+        playwright = sync_playwright().start()
+        browser = playwright.chromium.launch(headless=False, slow_mo=1500)
+        self.page = browser.new_page()
+        self.page.goto(self.url)
+
+        yield self.page
+
+        browser.close()
+        playwright.stop()
+
+    def test_task1_output(self, select, fill_context):
+        select(self.page, "//div[contains(@class, 'top-card') and .//h5[text()='Elements']]")
+        select(self.page, "//li[contains(@class, 'btn-light') and .//span[text()='Text Box']]")
